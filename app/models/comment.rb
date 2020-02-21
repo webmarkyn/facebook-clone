e8f5e400543b836e7d88dc3284473a2d8544a4f2
webmarkyn <web.markyn@gmail.com>
2020-02-22 00:17:22 +0300

    restyling done

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Comment < ApplicationRecord
-  validates :content, length: { minimum: 10, maximum: 50 }
+  validates :content, length: { minimum: 5, maximum: 50 }
   belongs_to :user
   belongs_to :post
 end
